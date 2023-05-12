261a808f46906e6bdc37f46cfea2c404f9b51ece
Abdullah Darwish <abdullahali88@gmail.com>
2023-05-12 16:57:06 +0300

    expanding submittion form

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-document.getElementsByClassName("idea idea1")[0].addEventListener("click", function(){
-    window.location.href = "idea1Expanded.html";  
+document.getElementsByClassName("idea idea1")[0].addEventListener("click", function () {
+    window.location.href = "idea1Expanded.html";
 })
-document.getElementsByClassName("idea idea2")[0].addEventListener("click", function(){
-    window.location.href = "idea2Expanded.html";  
+document.getElementsByClassName("idea idea2")[0].addEventListener("click", function () {
+    window.location.href = "idea2Expanded.html";
 })
+
+
+
